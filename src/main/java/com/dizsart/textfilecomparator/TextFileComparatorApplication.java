aaf1bd547fc475b23bbc620675ecd53f85a1b64b
Gadibia Daro <55360356+dizsart@users.noreply.github.com>
2022-02-16 11:02:02 +0100

    initial commit

---
+++ src/main/java/com/dizsart/textfilecomparator/TextFileComparatorApplication.java
@@ -0,0 +1,13 @@
+package com.dizsart.textfilecomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextFileComparatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextFileComparatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dizsart/textfilecomparator/TextFileComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizsart.textfilecomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextFileComparatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
