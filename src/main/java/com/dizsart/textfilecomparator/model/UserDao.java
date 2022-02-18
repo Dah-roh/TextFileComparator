9b3af95574d7616ffebb508e0d344c3928a4cb0d
Gadibia Daro <55360356+dizsart@users.noreply.github.com>
2022-02-18 08:16:43 +0100

    add text-comparison-feature

--- src/main/java/com/dizsart/textfilecomparator/model/UserDao.java
+++ src/main/java/com/dizsart/textfilecomparator/model/UserDao.java
@@ -2,7 +2,6 @@ package com.dizsart.textfilecomparator.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -10,8 +9,6 @@ public class UserDao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
-    @NotNull(message = "Username already in exist")
     private String username;
     @Column
     @JsonIgnore
